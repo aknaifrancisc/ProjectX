c6358b19d1975f9d027969f1bd0b59f3615bc56b
Francisc Aknai <francisc@aknai.net>
2014-08-31 20:50:39 +0300

    added vendor files and more styleing

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,11 @@
-//= require_tree .
+//= require js/bootstrap.min
+//= require js/smooth-scroll
+//= require js/custom
+//= require js/bind-polyfill
+
+
+$(document).ready(function() {
+  if(typeof console == 'undefined') {
+    this.console = { log: function() { } };
+  }
+});
