6b3a83802e555f64519c17bc8c4c9a6dbf8dba17
Francisc Aknai <francisc@aknai.net>
2014-08-31 20:07:21 +0300

    config gems

--- config.rb
+++ config.rb
@@ -1,72 +1,34 @@
-###
-# Compass
-###
+helpers do
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+end
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
-# Build-specific configuration
+ready do
+    sprockets.append_path 'vendor'
+end
+
+activate :livereload
+
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
+  activate :minify_javascript
+  activate :cache_buster
+  activate :relative_assets
+  # activate :favicon_maker,
+  # :favicon_maker_base_image => "favicon_base.svg"
+end
 
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+activate :deploy do |deploy|
+  deploy.method = :rsync
+  deploy.host   = "www.example.com"
+  deploy.user   = "example"
+  deploy.path   = "/public_html"
+  
+  # Optional Settings
+  deploy.port  = 22
+  deploy.clean = true
+  deploy.build_before = true
 end
